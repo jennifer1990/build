48a7ba1fc93a66e12b896f280bcc2bce1e410ee1
刘丽辉 <liulihui@kashuo.com>
2016-12-06 18:46:45 +0800

    单选组件  暂未完成

--- dev/js/components/Radio.vue
+++ dev/js/components/Radio.vue
@@ -1,9 +1,23 @@
 <template>
-
-	<div class="radio">
-        <input type="radio" id="radioMale">
-        <label class="ui-radio" for="radioMale"></label>
-        <label for="radioMale">男款</label>
-    </div>
-
+	<label class="H-radio">
+		<span class="H-radio-input" :class="{'disabled':disabled, 'checked':model===label}">
+			<span class="H-radio-inner"></span>
+			<input type="radio" class="H-radio-original" :value="label" v-model="model" :name="name">
+		</span>
+		{{model===label}}
+		{{model}}
+		{{label}}
+		<span class="H-radio-label">
+			<slot></slot>
+		</span>
+	</label>
 </template>
+<script type="text/javascript">
+	export default{
+		props:{
+			label:{},
+			disabled: Boolean,
+			name: String
+		}
+	}
+</script>
--- dev/js/views/radio/radio.vue
+++ dev/js/views/radio/radio.vue
@@ -1,7 +1,21 @@
 <template>
-	<radio></radio>
 
-	<div>
-		<span>sadasd</span>
+	<br>
+	<br>
+	<br>
+
+	<div style="margin: 100px;">
+		<radio name="a" :model="" label="1"></radio>
+		<radio name="a" :model="" label="2"></radio>
 	</div>
+	
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        radio: '1'
+      }
+    }
+  }
+</script>
